493c9638e3d49ec729e1b73d2aa4769906129777
jamsheerply <jamsheerpayyoli@gmail.com>
2024-08-26 12:32:48 +0530

    refact:course addreess addning validaition issue

--- config/dataBase.js
+++ config/dataBase.js
@@ -1,8 +1,28 @@
-require("dotenv").config()
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 try {
-    mongoose.connect(process.env.DB).then(()=>console.log("Connected to MongoDB"));
+  mongoose
+    .connect(process.env.DB)
+    .then(() => console.log("Connected to MongoDB"));
 } catch (error) {
-    console.error("Mongoose connection error:", error);
+  console.error("Mongoose connection error:", error);
 }
+
+// const mongoose = require("mongoose");
+
+// const url = process.env.DB;
+
+// const connectionParams = {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// };
+// mongoose
+//   .connect(url, connectionParams)
+//   .then(() => {
+//     console.log("Connected to database ");
+//   })
+//   .catch((err) => {
+//     console.error(`Error connecting to the database. \n${err}`);
+//   });
